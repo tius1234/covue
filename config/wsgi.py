60d85d1d80bf27f0b42702642466563c9d262c03
tius1234 <tius1234@nate.com>
2018-04-20 23:25:59 +0900

    DJANGO_SETTINGS_MODULE 을 .env 파일을 이용할 수 있도록 수정.

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+from .settings.base import env
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", env("DJANGO_SETTINGS_MODULE"))
 
 application = get_wsgi_application()
